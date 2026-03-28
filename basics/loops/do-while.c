631afd253a0244beaa69fefcca25e763ff2d679b
japaafk <matheushirose4@gmail.com>
2026-03-28 15:18:25 -0300

    do-while structure

--- basics/loops/do-while.c
+++ basics/loops/do-while.c
@@ -7,7 +7,7 @@ int main(void) {
         printf("What is n? ");
         scanf("%i", &n);
     }
-    while (n < 0);
+    while (n < 0); // the while condition here will be checked at last instead of the beginning
 
     for(int i = 0; i < n; i++) {
         printf("hello\n");
