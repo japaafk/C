00b716c38b7314b7ec277586a4a75e3d339fd687
japaafk <matheushirose4@gmail.com>
2026-03-29 17:23:34 -0300

    for structure in C

---
+++ basics/loops/for2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    for(int height = 0; height< 3; height++) {
+        for(int row = 0; row < 3; row++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
