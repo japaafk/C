252a81a3cbb59cf5708fab45bf34541314f025b6
japaafk <matheushirose4@gmail.com>
2026-03-29 22:19:52 -0300

    primitive tips

---
+++ basics/primitive tips/primitive_tips.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    float number1 = 7.34;
+    int number2 = 5;
+    double number3 = 7.432;
+    long number4 = 244534;
+    char letter = 'a';
+    char name[] = "Michael Jackson";
+
+    printf("%f\n %i\n %lf\n %li\n %c\n %s", number1, number2, number3, number4, letter, name);
+}
