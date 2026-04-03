e0bad3aabb1eda5d953f302869dceaae06f2dab9
japaafk <matheushirose4@gmail.com>
2026-04-03 16:26:10 -0300

    array of string

--- array/array3.c
+++ array/array3.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    // Here you are only storing the address
-    char *word[2]; // You can't mutate them'
+   /* Here you are only storing the address
+    char *word[2]; You can't mutate them'
     word[0] = "HI!";
     word[1] = "BYE!";
+    */
+    
+    char word[2][10]; // Flexible way to use strings in an array
+    strcpy(word[0], "HI!");
+    strcpy(word[1], "BYE!");
 
     printf("%s\n", word[0]);
     printf("%s\n", word[1]);
