188b131541a43b57cdbe6d1833e33bcacd970928
japaafk <matheushirose4@gmail.com>
2026-04-03 13:04:30 -0300

    type conversion

---
+++ basics/converter/converter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // Int to Float
+    int number1 = 15;
+    printf("%f\n", number1/(float) 4);
+
+    // Float to Int
+    float number2 = 13.5;
+    printf("%i\n", (int) number2/ 2);
+}
