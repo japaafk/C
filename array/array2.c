ead89a1c1f06c557053138432be574d5d715b1dc
japaafk <matheushirose4@gmail.com>
2026-04-03 14:22:28 -0300

    average grade calculation

---
+++ array/array2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int average(int length, int array[]) {
+    int sum = 0;
+    int result;
+    for(int numbers = 0; numbers < length; numbers++)
+    {
+        sum += array[numbers];
+    }
+    result = sum / length;
+    return result;
+}
+
+int main(void) {
+    
+    const int N = 3;
+    int scores[N];
+    for(int i = 0; i < N; i++) {
+        printf("Type ur score: ");
+        scanf("%i", &scores[i]);
+    }
+    printf("%i", average(N, scores));
+}
