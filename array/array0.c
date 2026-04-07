232f145611bfdba9234b96822b3b6477af80d658
japaafk <matheushirose4@gmail.com>
2026-04-06 23:16:19 -0300

    array syntax

---
+++ array/array0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    // instantiation syntax
+    int numbers[] = {3, 7, 8}; // In this case you can omit the size of the array
+
+    // individual element syntax
+    char letters[5];
+    letters[0] = 'H';
+    letters[1] = 'e';
+    letters[2] = 'l';
+    letters[3] = 'l';
+    letters[4] = 'o';
+
+}
