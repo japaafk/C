12421e58c10621f50378625742e7ef920f81ad36
japaafk <matheushirose4@gmail.com>
2026-04-10 00:09:08 -0300

    deep learning in algorithms

---
+++ algorithms/search.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    // string pointer - can't read whitespace
+    char *string[] = {"battleship", "boot", "cannon", "iron", "term", "thimble"};
+
+    char s[50];
+    printf("String: ");
+    scanf("%s", s);
+    for(int i = 0; i < 6; i++) {
+       // strcmp will return 0 if the strings are equal, otherwise it will return a positive number or negative number  
+        if (strcmp(string[i], s) == 0) { 
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
