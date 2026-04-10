f3bd07be678c645fc8691643c6c6452140e7afc1
japaafk <matheushirose4@gmail.com>
2026-04-09 23:45:25 -0300

    reading strings

--- basics/input/input2.c
+++ basics/input/input2.c
@@ -2,12 +2,15 @@
 
 int main(void) {
 
-    char name1[50] = "Yago Ryan";
+    char name3[50] = "Yago Ryan";
     char name2[50];
+    char name1[50];
 
-    printf("What's your name? \n");
+    printf("What's the name of the first participant?  \n");
     fgets(name2, 50, stdin); // stdin reads the inputs from your keyboard
+    printf("And what is the name of the second participant? ");
+    scanf("%s", name1); // scanf can also read strings, but it stops reading at whitespace.
 
-    printf("%s vs %s\n", name1, name2);
+    printf("%s vs %s vs %s\n", name1, name2, name3);
 
 }
